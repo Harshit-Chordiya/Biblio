148c499d13ba957f1f0a20d8f9435760937ec973
Harshit-Chordiya <harshitchordiya03@gmail.com>
2024-01-28 02:19:33 +0530

    Page router -> Next Router started

---
+++ src/app/genres/[slug]/page.js
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/navigation'
+import Header from '@/components/global/Header'
+import Footer from '@/components/global/Footer'
+import UnsupportedRoute from '@/components/global/UnsupportedRoute'
+
+const Slug = () => {
+  const router = useRouter()
+  const { slug } = router.query
+
+  return (
+    <div>
+      <div className='bg-gradient-to-tr from-rose-50 to-rose-200 dark:bg-gradientedge text-gray-900 dark:text-gray-100 min-h-screen'>
+        <Header />
+        <UnsupportedRoute url={`https://www.goodreads.com/genres/${slug}`} />
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Slug
